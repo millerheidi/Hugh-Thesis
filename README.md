85c07281919881752fa38166278e3490228b8ca5
Hugh Morison <hugh.morison@hotmail.ca>
2018-11-05 20:47:54 -0500

    Fleshed out network structure (generator implementation)

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Neural Network Architectures in Silicon Photonic Networks
 
 Hugh Morison | Engineering Physics (Computing Option) Bachelor's Thesis | Queen's University
+
+At this point, code is all still under active development, and I am using git access my files on many machines.
+This REDAME will be updated when code is deployment ready.
--- SiPhotoNet/photonic_network.py
+++ SiPhotoNet/photonic_network.py
@@ -24,23 +24,22 @@ import copy
 
 class network:
     def __init__(self, weights, wavelengths = [1550e-9], powers = [1]):
+        self.N = len(wavelengths)
         self.neurons = []
         self.waveguide = []
         for i,weightbank in enumerate(weights):
-            self.neurons.append(photoneuron.neuron(weightbank, wavelengths[i]))
+            self.neurons.append(photoneuron.neuron(weightbank, wavelengths[i], output_power = powers[i]))
             self.waveguide.append({'wavelength': wavelengths[i], 'power': powers[i]})
     
-    def advance(self): #TODO
-        new_waveguide = copy.deepcopy(self.waveguide)
+    def advance(self):
+        split_waveguide = copy.deepcopy(self.waveguide)
+        for signal in split_waveguide:
+            signal['power'] = signal['power']/self.N
         for i,neuron in enumerate(self.neurons):
-            new_waveguide[i]['power'] = neuron.nextState(self.waveguide)
-        self.waveguide = new_waveguide
+            self.waveguide[i]['power'] = neuron.act(split_waveguide)
+         
+    def simulate(self, input_signal = None): # generator will yield new values infinitely*
+        while True:
+            self.advance() 
+            yield
             
-    def simulate(self): #TODO
-        done = False
-        while not done:
-            self.advance() # advance state of system
-            # do any plotting/maniupulation of the data
-            # done = True when done
-            break
-        return
